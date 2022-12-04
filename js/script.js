49ebf95ab8e4016c93bfd8952d646d6984b1b3bb
stepankniazevich <steoan2000kniaz@outlook.com>
2022-12-04 23:51:19 +0200

    add footer styles

--- js/script.js
+++ js/script.js
@@ -28,4 +28,21 @@ var swiper = new Swiper(".slider", {
       clickable: true,
     },
   });
-  
+
+  //----------------Acordion--------------------------------
+let acc = document.getElementsByClassName("category__accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this
+            .classList
+            .toggle("footer__active");
+        let panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight +"px";
+        }
+    });
+}
